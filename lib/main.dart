244440a0902f38867d2f71ff621ef7f102c92371
3laaOmran <115973437+3laaOmran@users.noreply.github.com>
2024-12-08 20:58:30 +0200

    Initial project Added empty home screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/Home/home_screen.dart';
+
+void main() {
+  runApp(const IslamiApp());
+}
+
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
