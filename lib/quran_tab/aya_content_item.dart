75718f353d9522791c9c8f473b9c1951d637a613
3laaOmran <115973437+3laaOmran@users.noreply.github.com>
2024-12-13 18:59:30 +0200

    Added radio tab. Refactor quran tab. Refactor sura content

--- lib/quran_tab/aya_content_item.dart
+++ lib/quran_tab/aya_content_item.dart
@@ -3,28 +3,50 @@ import 'package:islami_app/styles/text_styles.dart';
 
 import '../themes/app_colors.dart';
 
-class AyaContentItem extends StatelessWidget {
+class AyaContentItem extends StatefulWidget {
   final String content;
   final int index;
 
   const AyaContentItem({super.key, required this.content, required this.index});
 
   @override
+  State<AyaContentItem> createState() => _AyaContentItemState();
+}
+
+class _AyaContentItemState extends State<AyaContentItem> {
+  int? selectedAyaIndex;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 4),
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          border: Border.all(
-            color: AppColors.primaryColor,
-            width: 1,
-          )),
-      child: Text(
-        '$content [${index + 1}]',
-        textAlign: TextAlign.center,
-        textDirection: TextDirection.rtl,
-        style: TextStyles.font20BoldPrimaryColor,
+    return InkWell(
+      onTap: () {
+        setState(() {
+          if (selectedAyaIndex == widget.index) {
+            selectedAyaIndex = null;
+          } else {
+            selectedAyaIndex = widget.index;
+          }
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 4),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+        decoration: BoxDecoration(
+            color: selectedAyaIndex == widget.index
+                ? AppColors.primaryColor
+                : AppColors.transparent,
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(
+              color: AppColors.primaryColor,
+              width: 1,
+            )),
+        child: Text(
+          '${widget.content} [${widget.index + 1}]',
+          textAlign: TextAlign.center,
+          textDirection: TextDirection.rtl,
+          style: selectedAyaIndex == widget.index
+              ? TextStyles.font20BoldBlackColor
+              : TextStyles.font20BoldPrimaryColor,
+        ),
       ),
     );
   }
